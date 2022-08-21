0189fb6d95fca04fe6ee55a8750717659643a3b0
gauthamsathyan <gauthamsathyan@gmail.com>
2022-08-22 02:16:20 +0530

    Add newfile

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -28,6 +28,10 @@ projects = [
 def home():
     return render_template("home.html", projects = projects)
 
+@app.route('/about')
+def about():
+    return render_template("about.html", projects = projects)
+
 @app.route('/contact')
 def contact():
     return render_template("contact.html") 
