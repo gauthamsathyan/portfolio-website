64672a8f016cf8b833174b50dcc5bcb191f185dc
gauthamsathyan <gauthamsathyan@gmail.com>
2022-08-22 02:00:39 +0530

    Update

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -28,10 +28,6 @@ projects = [
 def home():
     return render_template("home.html", projects = projects)
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
-
 @app.route('/contact')
 def contact():
     return render_template("contact.html") 
