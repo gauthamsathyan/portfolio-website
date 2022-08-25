39dbd3ef6329872fdf348063773e0a9468ecdaf3
gauthamsathyan <gauthamsathyan@gmail.com>
2022-08-25 22:24:11 +0530

    Update

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -6,31 +6,50 @@ app = Flask(__name__)
 projects = [
     {
         "name": "Glaucoma detection from Retinal Fundus Images",
-        "thumb": "img/glaucoma.png", # Thumbnail for home
+        "thumb": "img/glaucoma.jpg", # Thumbnail for home
         "categories": ["IEEE", "Research Paper"],
         "prod": "https://ieeexplore.ieee.org/document/9182388"
     },
     {
         "name": "Personal finance tracking app with React",
-        "thumb": "img/glaucoma.png",
+        "thumb": "img/glaucoma.jpg",
         "categories": ["react", "javascript"],
         "prod": "https://youtube.com"
     },
     {
         "name": "REST API Documentation with Postman and Swagger",
-        "thumb": "img/glaucoma.png",
+        "thumb": "img/glaucoma.jpg",
         "categories": ["writing"],
         "prod": "https://twitter.com"
     }
+]
+
+# ALL THE DIMENSIONS OF IMAGE SHOULD BE 1920X1080
+certifications = [ 
+    {
+        "name": "AI Engineering",
+        "thumb": "img/ai-engineer.jpg",
+        "prod": "https://www.credly.com/badges/79dfbd63-5141-4e7a-972a-6e45ff53a089/public_url"
+    },
+    {
+        "name": "Algorithmic Toolbox",
+        "thumb": "img/algorithm.jpg",
+        "prod": "https://www.coursera.org/account/accomplishments/certificate/LWCU3CWG7GRG"
+    },
+    {
+        "name": "TensorFlow Developer",
+        "thumb": "img/tensorflow.jpg",
+        "prod": "https://www.coursera.org/account/accomplishments/specialization/certificate/AYXSTXBMNW9J"
+    },
 ]
 
 @app.route('/')
 def home():
     return render_template("home.html", projects = projects)
 
-@app.route('/about')
-def about():
-    return render_template("about.html", projects = projects)
+@app.route('/certification')
+def certification():
+    return render_template("certification.html", certifications = certifications)
 
 @app.route('/contact')
 def contact():
